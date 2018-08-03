027987c3fcb73a912e79eb92a5f71c5b2921ce28
hai <hilin2333@gmail.com>
2018-08-03 14:02:25 +0800

    Initial Commit

---
+++ README.md
@@ -0,0 +1,6 @@
+#### overview
+| 技术| 版本 |
+| :------ | :------ |
+| graphql-java-tools | `5.2.3` |
+| graphql-spring-boot-starter | `5.0.1` |
+| rxjava2 | `2.1.5` |
---
+++ src/main/java/com/bdgx/resolvers/Subscription.java
@@ -0,0 +1,23 @@
+package com.bdgx.resolvers;
+
+import com.bdgx.publishers.StockTickerPublisher;
+import com.coxautodev.graphql.tools.GraphQLSubscriptionResolver;
+import org.reactivestreams.Publisher;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Component
+class Subscription implements GraphQLSubscriptionResolver {
+
+    private StockTickerPublisher stockTickerPublisher;
+
+    Subscription(StockTickerPublisher stockTickerPublisher) {
+        this.stockTickerPublisher = stockTickerPublisher;
+    }
+
+    Publisher<StockPriceUpdate> stockQuotes(List<String> stockCodes) {
+        return stockTickerPublisher.getPublisher(stockCodes);
+    }
+
+}
